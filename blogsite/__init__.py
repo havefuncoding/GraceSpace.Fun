1d3d709e77cec222d3e5698100aa6cc5a6dcc823
Grace Nellore <gracernellore@gmail.com>
2019-11-22 20:36:36 -0800

    update

---
+++ blogsite/__init__.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'f3f7cb198c699468c3d09c20d59e7b62'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL')
+app.config['MAIL_PASSWORD'] = os.environ.get('PASSWORD')
+mail = Mail(app)
+
+# IMPORTANT - Leave this import below db setup, workaround to exception from circular importing
+from blogsite import routes
